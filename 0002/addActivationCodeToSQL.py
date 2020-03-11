dff6370036a4e47630e29685952cc59c35d072d6
gaoliujiadi <gaoliujiadi@gmail.com>
2020-03-11 13:55:34 +0800

    chang the activation code numbers

--- 0002/addActivationCodeToSQL.py
+++ 0002/addActivationCodeToSQL.py
@@ -10,7 +10,7 @@ cursor = SQL.cursor()#建立游标对象
 ls_o = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','@','$','%','&','?',]
 ls = ls_o
 lsAC = []
-for i in range(10):
+for i in range(200):
     random.shuffle(ls)
     ls_1 = ls[:10]
     AC = ''.join(ls_1)
